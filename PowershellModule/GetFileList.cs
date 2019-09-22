d49092872a6a2b5b51bc48b327b748d98e470659
woobsDev <RobPhi84@gmail.com>
2019-09-21 23:20:52 -0400

    Add project files.

---
+++ PowershellModule/GetFileList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Management.Automation;
+
+namespace PowershellModule
+{
+    [Cmdlet(VerbsCommon.Get, "FileList")]
+    public class GetFileList : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true,
+            Position = 0,
+            HelpMessage = "Path to retrieve files in directory.")]
+        public string Path { get; set; }
+
+        [Parameter(
+            Mandatory = false,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true,
+            Position = 1,
+            HelpMessage = "List directories")]
+        public SwitchParameter ListDirectories { get; set; }
+
+        private bool listDirectorySwitchValue;
+
+        protected override void BeginProcessing()
+        {
+            listDirectorySwitchValue = ListDirectories.IsPresent;
+        }
+
+        protected override void ProcessRecord()
+        {
+            switch(listDirectorySwitchValue)
+            {
+                case true:
+                    if(!Directory.Exists(Path))
+                    {
+                        WriteObject(this.SessionState.Path.CurrentLocation.ToString());
+                    }
+                    else
+                    {
+                        WriteObject(Directory.GetDirectories(Path));
+                    }
+                    break;
+
+                case false:
+                    if (!Directory.Exists(Path))
+                    {
+                        WriteObject(Directory.GetFiles(this.SessionState.Path.CurrentLocation.ToString()));
+                    }
+                    else
+                    {
+                        //WriteObject(Directory.GetDirectories(path));
+                        WriteObject(Directory.GetFiles(Path));
+                    }
+                    break;
+
+            }
+
+            
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+    }
+}
---
+++ PowershellModule/RenameFileListToJpeg.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+namespace PowershellModule
+{
+    [Cmdlet(VerbsCommon.Rename, "FileListToJpeg")]
+    public class RenameFileListToJpeg : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            ValueFromPipeline = true,
+            Position = 0,
+            HelpMessage = "Path to copy files from Localstate\\Assets and rename.")]
+        public string DestinationPath { get; set; }
+
+        string createDirectoryChoice;
+        string systemDirectoryWithPictures;
+        string[] filesToCopy;
+
+        protected void CopyFilesAndRename(string sourceFile, string destination)
+        {
+            try
+            {
+                File.Copy(sourceFile, Path.Combine(destination, Path.GetFileName(sourceFile) + ".jpeg"));
+            }
+            catch (IOException err)
+            {
+                WriteObject(err);
+            }
+        }
+
+        protected override void BeginProcessing()
+        {
+            //this.DestinationPath = @"C:\temp\wallpaperDesktops\";
+            this.systemDirectoryWithPictures = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+                + @"\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets";
+            //this.Path = this.SessionState.Path.CurrentLocation.ToString();
+
+            this.filesToCopy = Directory.GetFiles(systemDirectoryWithPictures);
+        }
+
+        protected override void ProcessRecord()
+        {
+            /* Test path where files reside
+            WriteObject(systemDirectoryWithPictures);
+            WriteObject(DestinationPath.ToString());
+            */
+            if(!Directory.Exists(DestinationPath))
+            {
+                WriteWarning(DestinationPath+" doesn't exist.  Create directory(Y\\N)?");
+                createDirectoryChoice = Console.ReadLine();
+
+                if (createDirectoryChoice == "y" || createDirectoryChoice == "Y")
+                {
+                    Directory.CreateDirectory(DestinationPath);
+                    WriteObject(DestinationPath + " created.");
+
+
+                    WriteObject("Copying from known locations containing desktop wallpapers to " + DestinationPath);
+                    foreach (string file in filesToCopy)
+                    {
+                        CopyFilesAndRename(file, DestinationPath);
+                    }
+                }
+                else if (createDirectoryChoice == "n" || createDirectoryChoice == "N")
+                {
+                    WriteObject(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)+"\\Microsoft\\Windows\\Themes");
+                    WriteObject("Exiting...");
+                    return;
+                }
+                
+                
+            }
+            else
+            {
+                WriteObject("Copying items to " + DestinationPath);
+                foreach (string file in filesToCopy)
+                {
+                    CopyFilesAndRename(file, DestinationPath);
+                }
+            }
+
+            // Copy single file.  No iteration needed here.
+            CopyFilesAndRename((Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Microsoft\\Windows\\Themes\\TranscodedWallpaper").ToString(), DestinationPath);
+
+        }
+
+        protected override void StopProcessing()
+        {
+            base.StopProcessing();
+        }
+    }
+}
